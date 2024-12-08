169e44bee6fdfcbb3301bfe6b5c7ca2078d0f6c9
小林淳司 <kbys@kobayashiatsushinonotobukkukonpyuta.local>
2024-12-08 17:55:59 +0900

    setmax

--- webapp/go/main.go
+++ webapp/go/main.go
@@ -4,8 +4,8 @@ import (
 	crand "crypto/rand"
 	"encoding/json"
 	"fmt"
-	"log/slog"
 	"log"
+	"log/slog"
 	"net"
 	"net/http"
 	_ "net/http/pprof"
@@ -23,8 +23,8 @@ var db *sqlx.DB
 
 func main() {
 	go func() {
-        log.Fatal(http.ListenAndServe(":6060", nil))
-    }()
+		log.Fatal(http.ListenAndServe(":6060", nil))
+	}()
 	mux := setup()
 	slog.Info("Listening on :8080")
 	http.ListenAndServe(":8080", mux)
@@ -70,6 +70,9 @@ func setup() http.Handler {
 		panic(err)
 	}
 	db = _db
+
+	db.SetMaxOpenConns(100) // Set the maximum number of open connections to 100
+	db.SetMaxIdleConns(100) // Set the maximum number of idle connections
 
 	mux := chi.NewRouter()
 	// mux.Use(middleware.Logger)
